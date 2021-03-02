018dfe62b1655280d7cb97738386662611a91d7b
apine <cmassaro@wikimedia.org>
2021-03-02 13:12:00 -0800

    Add function-schemata as a submodule to function-orchestrator
    Add a convenience script to update commit hash for function-schemata
    
    Bug: T276364
    Change-Id: I2c4e98421af1f2802fefbbf5200b1fb2d95e3ce3

---
+++ update_local_submodule.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script changes the commit hash of the function-schemata submodule.
+# If you want to update the commit hash (e.g., in order to incorporate changes
+# to function-schemata into this repository), this script automates that process.
+
+if [ -z $1 ]; then
+    echo "Usage: update_local_submodule.sh <commit hash>"
+    exit 1
+fi
+COMMITHASH=$1
+
+# Removes function-schemata submodule.
+rm .gitmodules
+git add .gitmodules
+git submodule deinit -f function-schemata
+git rm --cached function-schemata
+rm -rf .git/modules/function-schemata
+git commit -m "Removed previous version of submodule."
+
+# Adds function-schemata at the desired commit.
+git submodule add https://gerrit.wikimedia.org/r/mediawiki/services/function-schemata
+git update-index --cacheinfo 160000,${COMMITHASH},function-schemata
