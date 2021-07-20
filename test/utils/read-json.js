8932b626f40a6163d140026314324e7bf4f18f9a
apine <cmassaro@wikimedia.org>
2021-07-20 10:18:15 -0500

    Split MSW-based tests into separate file to avoid cross-test interference.
    Bug: T287018
    Change-Id: I18eb817c4fa1b7eda1850dd8ecdbe4d36f3e13ed

---
+++ test/utils/read-json.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const fs = require('fs');
+
+function readJSON(fileName) {
+    return JSON.parse(fs.readFileSync(fileName, { encoding: 'utf8' }));
+}
+
+module.exports = { readJSON };
