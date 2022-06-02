2ac357732f49b2667e392d088b1cfd919e90d6be
apine <cmassaro@wikimedia.org>
2022-06-02 17:14:58 -0500

    Support the use of websockets for re-entrant function calls.
    Bug: T318359
    Depends-On: I010c2f7efe85b97fdbc6ce8c1a6e84e8fd664e41
    Change-Id: I6d6fe8daf5854cca4b0024beb186cce6308f7a81

--- src/Evaluator.js
+++ src/Evaluator.js
@@ -1,19 +1,39 @@
 'use strict';
 
 const fetch = require( '../lib/fetch.js' );
-const { convertZObjectToBinary } = require( '../function-schemata/javascript/src/serialize.js' );
+const { convertWrappedZObjectToVersionedBinary } = require( '../function-schemata/javascript/src/serialize.js' );
+const normalize = require( '../function-schemata/javascript/src/normalize.js' );
+const WebSocket = require( 'ws' );
+
+const AVRO_SCHEMA_VERSION_ = '0.0.2';
 
 /**
  * Function evaluator. Wraps API calls to the function-evaluator service, which
  * runs native code implementations.
  */
 class Evaluator {
-	constructor( evaluatorUri ) {
+	constructor( evaluatorWs, evaluatorUri, wikiUri, useReentrance, doValidate ) {
+		this.evaluatorWs_ = evaluatorWs;
 		this.evaluatorUri_ = evaluatorUri;
+		this.wikiUri_ = wikiUri;
+		this.doValidate_ = doValidate;
+		this.useReentrance_ = useReentrance;
+		this.timeout_ = 10000; // wait 10 seconds
 	}
 
 	async evaluate( functionCall ) {
-		const serialized = convertZObjectToBinary( functionCall );
+		if ( this.useReentrance_ ) {
+			return await this.evaluateReentrant_( functionCall );
+		} else {
+			return await this.evaluate_( functionCall );
+		}
+	}
+
+	async evaluate_( functionCall ) {
+		const serialized = convertWrappedZObjectToVersionedBinary( {
+			zobject: functionCall,
+			reentrant: this.useReentrance_
+		}, /* version= */ AVRO_SCHEMA_VERSION_ );
 		return await fetch(
 			this.evaluatorUri_, {
 				method: 'POST',
@@ -21,6 +41,50 @@ class Evaluator {
 				headers: { 'Content-type': 'application/octet-stream' }
 			}
 		);
+	}
+
+	/*
+     * Ignore the subsequence function for coverage purposes; we can't feasibly
+     * unit-test this functionality.
+     *
+     * TODO (T322056): Make EvaluatorStub able to recognize and handle multiple
+     * reentrant websocket-based function calls.
+     */
+	// istanbul ignore next
+	async evaluateReentrant_( functionCall ) {
+		const evaluatePromise = this.evaluate_( functionCall );
+		const client = new WebSocket( this.evaluatorWs_ );
+		client.on( 'open', () => {
+			console.log( 'WS connection opened' );
+		} );
+		client.on( 'message', async ( theMessage ) => {
+			theMessage = theMessage.toString();
+			console.log( 'message received:', theMessage );
+			if ( theMessage.startsWith( 'call' ) ) {
+				const orchestrate = require( './orchestrate' );
+				theMessage = theMessage.replace( /^call\s*/, '' );
+				const Z7 = JSON.parse( theMessage );
+				const toOrchestrate = {
+					zobject: Z7,
+					evaluatorWs: this.evaluatorWs_,
+					evaluatorUri: this.evaluatorUri_,
+					wikiUri: this.wikiUri_,
+					useReentrance: true,
+					doValidate: this.doValidate_
+				};
+				const callResult = await orchestrate( toOrchestrate );
+				const normalized = ( await normalize( callResult.Z22K1 ) ).Z22K1;
+				console.log( 'normalized is', normalized );
+				client.send( JSON.stringify( normalized ) );
+			}
+		} );
+		client.on( 'close', () => {
+			console.log( 'WS connection closed' );
+		} );
+		const result = await evaluatePromise;
+		// TODO: How to wait until connection opens?
+		client.close();
+		return result;
 	}
 }
 
