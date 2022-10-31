28825ebf47aaa874cd45a7af4025a23cd5920f41
dmartin <dmartin@wikimedia.org>
2022-10-30 21:02:48 -0700

    Record which implementation gets selected
    For composition and evaluated implementations, if they
    are indicated by a Z9, grab the persistent ID and arrange
    for it to show up in the metadata map.
    (Built-in implementations' persistent ID is already captured.)
    
    Bug: T320457
    Change-Id: Ie29138a88217eedd95fe916513357f7715ddae2a

--- src/implementationSelector.js
+++ src/implementationSelector.js
@@ -1,12 +1,15 @@
 'use strict';
 
+const { BuiltIn } = require( './implementation.js' );
+
 class RandomImplementationSelector {
 
+	// Each implementation is an instance of class Implementation.
 	select( implementations ) {
 		// TODO (T296677): Implement heuristics to decide which implementation to
 		// use. Implicitly, current heuristic is to use a builtin if available;
 		// otherwise, choose a random implementation and return that.
-		const builtin = implementations.find( ( impl ) => Boolean( impl.Z14K4 ) );
+		const builtin = implementations.find( ( impl ) => ( impl instanceof BuiltIn ) );
 		if ( builtin !== undefined ) {
 			return builtin;
 		}
