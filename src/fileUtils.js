b0e990a81e5bca11076ddaf676dcc3ebe085b86e
James D. Forrester <jforrester@wikimedia.org>
2023-04-18 12:14:53 -0400

    Empty repo, this project has moved to Wikimedia Gitlab
    Bug: T334397
    Change-Id: Ibaabaa10c768a34590df8382dc3ca53785a790a2

--- src/fileUtils.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fs = require( 'fs' );
-
-function readJSON( fileName ) {
-	return JSON.parse( fs.readFileSync( fileName, { encoding: 'utf8' } ) );
-}
-
-module.exports = { readJSON };
